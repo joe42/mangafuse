204274c68db60f6ba2918d2c9d017cc5f865882b
Johannes Müller <dna42@gmx.de>
2012-06-02 19:17:25 +0200

    Modify start_mangafuse.sh workaround start of cloudfusion does not work without foreground parameter

--- cloudfusion/start_mangafuse.sh
+++ cloudfusion/start_mangafuse.sh
@@ -2,8 +2,10 @@
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $CURRENT_DIR
 mkdir -p ~/.mangas  .cloudfusion/logs
-python -m cloudfusion.main  ~/.mangas >/dev/null 2>&1 &
+python -m cloudfusion.main  ~/.mangas foreground >/dev/null 2>&1 &
+sleep 1
 cp cloudfusion/config/mangafuse.ini ~/.mangas/config/config
+sleep 1
 mkdir  ~/.mangas/data/tower\ of\ god
 if [ ! -h ~/mangas ]
 then
