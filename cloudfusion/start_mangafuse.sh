634f1054069fafb6442e8828ac70563da609742d
Johannes Müller <dna42@gmx.de>
2012-06-02 17:51:03 +0200

    Modify start_mangafuse.sh, fix bad directory

--- cloudfusion/start_mangafuse.sh
+++ cloudfusion/start_mangafuse.sh
@@ -3,7 +3,7 @@ CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $CURRENT_DIR
 mkdir -p ~/.mangas  .cloudfusion/logs
 python -m cloudfusion.main  ~/.mangas 
-cp cloudfusion/config/mangafuse.ini ~/mangas/data/config/config
+cp cloudfusion/config/mangafuse.ini ~/.mangas/data/config/config
 mkdir  ~/.mangas/data/tower\ of\ god
 if [ ! -h ~/mangas ]
 then
