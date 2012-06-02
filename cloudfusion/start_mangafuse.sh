64dab41094c42e3e2074c9d62199b47cf007b2aa
Johannes Müller <dna42@gmx.de>
2012-06-02 18:58:50 +0200

    Modify start_mangafuse.sh, fix run file system in background

--- cloudfusion/start_mangafuse.sh
+++ cloudfusion/start_mangafuse.sh
@@ -2,7 +2,7 @@
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $CURRENT_DIR
 mkdir -p ~/.mangas  .cloudfusion/logs
-python -m cloudfusion.main  ~/.mangas 
+python -m cloudfusion.main  ~/.mangas >/dev/null 2>&1 &
 cp cloudfusion/config/mangafuse.ini ~/.mangas/config/config
 mkdir  ~/.mangas/data/tower\ of\ god
 if [ ! -h ~/mangas ]
