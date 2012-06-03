bc7ccf020bc471b5a9e3fa2856815d8f5753df42
Johannes Müller <dna42@gmx.de>
2012-06-03 13:45:55 +0200

    Add install script for ubuntu/debian

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+has_sudo=0
+user=`id -nu`
+
+if [ "`id -u`" == "0" ]; then
+  echo Execute this script as normal user and not as root.
+  exit
+if [ `which sudo > /dev/null` ]; then
+  sudo su
+else
+  su
+fi
+
+apt-get install git -y
+apt-get install python-setuptools -y
+git clone git://github.com/joe42/mangafuse.git
+chown -R $user:$user mangafuse
+cd mangafuse/cloudfusion/
+python setup.py install
