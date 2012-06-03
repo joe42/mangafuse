b09a4e87ccd425e3f3c7c64f340a552c4744c507
Johannes Müller <dna42@gmx.de>
2012-06-03 18:43:52 +0200

    Modify start script 	Make it start from anywhere Modify install script 	copy start script to binary directory to make it executable from anywhere

--- cloudfusion/start_mangafuse.sh
+++ cloudfusion/start_mangafuse.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $CURRENT_DIR
+PYTHON_PCK_PATH="`python -c 'import cloudfusion; print cloudfusion.__file__'`"
+CONFIG_DIR=`dirname "$PYTHON_PCK_PATH"`"cloudfusion/cloudfusion/config"
+
 mkdir -p ~/.mangas  .cloudfusion/logs
 python -m cloudfusion.main  ~/.mangas foreground >/dev/null 2>&1 &
 sleep 1
-cp cloudfusion/config/mangafuse.ini ~/.mangas/config/config
+cp "$CONFIG_DIR"/mangafuse.ini ~/.mangas/config/config
 sleep 1
 mkdir  ~/.mangas/data/tower\ of\ god
 if [ ! -h ~/mangas ]
