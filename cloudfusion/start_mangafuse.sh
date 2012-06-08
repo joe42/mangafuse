d1527f5b7326a1832eeb900cda967c7ba7d66827
Johannes Müller <dna42@gmx.de>
2012-06-09 00:53:34 +0200

    Fix start script 	id needs user as argument 	if condition for checking string for zero is -z 	ill placed exit statement

--- cloudfusion/start_mangafuse.sh
+++ cloudfusion/start_mangafuse.sh
@@ -2,15 +2,15 @@
 PYTHON_PCK_PATH="`python -c 'import cloudfusion; print cloudfusion.__file__'`"
 CONFIG_DIR=`dirname "$PYTHON_PCK_PATH"`"/config"
 
-has_fuse_group="`id -Gn|grep " fuse "`"
-if [ -n "$has_fuse_group" ]; then
+user="`id -un`"
+has_fuse_group="`id "$user" -Gn|grep " fuse "`"
+if [ -z "$has_fuse_group" ]; then
 	echo "Please, type root's password..."
 	if [ `which sudo > /dev/null` ]; then
 		sudo "usermod -a -G fuse "`id -un`
 	else
 		su -c "usermod -a -G fuse "`id -un`
 	fi
-	exit
 fi
 mkdir -p ~/.mangas  .cloudfusion/logs
 python -m cloudfusion.main  ~/.mangas foreground >/dev/null 2>&1 &
